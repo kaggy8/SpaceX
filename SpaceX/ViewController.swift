b25d94f5f2f7928305d6499b150fd4ad434c63e7
Stanislav <kaggy8@ya.ru>
2022-07-18 22:14:29 +0300

    Initial Commit

---
+++ SpaceX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Stanislav Demyanov on 18.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
