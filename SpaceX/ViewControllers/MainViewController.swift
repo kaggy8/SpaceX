50b42c8f78616483f8c27a9bd11b43b2b2e88211
Stanislav <kaggy8@ya.ru>
2022-07-27 00:34:56 +0300

    Work in progress...

--- SpaceX/ViewControllers/MainViewController.swift
+++ SpaceX/ViewControllers/MainViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    @IBOutlet weak var imageRocket: UIImageView!
     @IBOutlet weak var mainScrollView: UIScrollView!
     @IBOutlet weak var nameRocketLabel: UILabel!
     @IBOutlet weak var firstLaunchDateLabel: UILabel!
@@ -43,6 +44,7 @@ extension MainViewController {
         networkManager.getData { result in
             self.rockets = result
             self.setUI()
+            self.getImage()
         }
     }
     
@@ -58,11 +60,37 @@ extension MainViewController {
         dateF.dateFormat = "dd MMMM, yyyy"
         let finishedDate = dateF.string(from: date)
         
-        
+        let locale = Locale(identifier: "en_US")
+        let countryCode = locale.countryCode(by: rockets[1].country)
+        print(countryCode as Any)
+        let multiLocale = Locale(identifier: countryCode ?? " ")
+        let nameCountry = multiLocale.localizedString(forRegionCode: countryCode ?? " ")
+        let ss = NSLocale.current.localizedString(forRegionCode: countryCode ?? " ")
+        print(nameCountry as Any)
+        print(Locale.isoRegionCodes)
         
         nameRocketLabel.text = rockets.first!.name
         firstLaunchDateLabel.text = finishedDate
         countryLaunchLabel.text = rockets.first!.country
         costPerLaunchLabel.text = costPerLaunch
+    }
+    
+    private func getImage() {
+        guard let urlString = rockets[0].flickrImages.randomElement() else { return }
+        let url = URL(string: urlString)
+        let data = try? Data(contentsOf: url!)
+        imageRocket.image = UIImage(data: data!)
+        imageRocket.contentMode = .scaleAspectFill
+    }
+}
+
+extension Locale {
+    func countryCode(by countryName: String) -> String? {
+        return Locale.isoRegionCodes.first(where: { code -> Bool in
+            guard let localizedCountryName = localizedString(forRegionCode: code) else {
+                return false
+            }
+            return localizedCountryName.lowercased() == countryName.lowercased()
+        })
     }
 }
